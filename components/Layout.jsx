4611c202d0df55f492826d42606efd2f3746975e
rinn-dev <mr.htetmyatlinn404@gmail.com>
2022-10-20 01:01:57 +0630

    Fixed Z-indexes

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -21,7 +21,7 @@ const Layout = ({ children }) => {
   return (
     <div className="flex flex-col xl:flex-row bg-rinn-primary">
       <div
-        className={`z-10 overflow-y-auto xl:overflow-hidden border-2 border-rinn-border  items-start w-screen fixed xl:sticky top-0 bottom-0 left-0 xl:translate-x-0 flex bg-rinn-primary xl:w-[25%] xl:max-h-screen ${
+        className={`z-20 overflow-y-auto xl:overflow-hidden border-2 border-rinn-border  items-start w-screen fixed xl:sticky top-0 bottom-0 left-0 xl:translate-x-0 flex bg-rinn-primary xl:w-[25%] xl:max-h-screen ${
           !isSidebarOpen
             ? "-translate-x-full animate-slideOut"
             : "translage-x-0 animate-slideIn"
